9c2c4808142c1a9a8a552630fba7697081be02c0
ArturoZAC <arturoyz2105@gmail.com>
2024-12-05 11:09:17 -0500

    commitAll

---
+++ tests/app.test.ts
@@ -0,0 +1,11 @@
+describe('Test in the App File', () => {
+  test('should be true', () => {
+    const num1 = 10;
+    const num2 = 20;
+
+    const result = num1 + num2;
+
+    expect( result ).toBe(30)
+
+  })
+})
